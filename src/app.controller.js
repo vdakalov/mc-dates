a03bca7940d19615c5f2032d743226bee57a998f
Viktor Dakalov <atlantidu@gmail.com>
2018-03-21 22:23:14 +0500

    Добавил событие изменения дат и поправил интерфейс компонента

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,4 +3,8 @@ angular
   .controller('MainController', ['$scope', function MainController($scope) {
     $scope.from = new Date();
     $scope.to = new Date();
+
+    $scope.onChange = function onChange() {
+      alert(`${$scope.from && $scope.from.toLocaleDateString('ru')} - ${$scope.to && $scope.to.toLocaleDateString('ru')}`)
+    };
   }]);
--- src/mc-dates/mc-dates.component.js
+++ src/mc-dates/mc-dates.component.js
@@ -3,43 +3,65 @@ angular
   .component('mcDates', {
     templateUrl: 'mc-dates/mc-dates.template.html',
     bindings: {
-      from: '=',
-      to: '='
+      dateFrom: '=',
+      dateTo: '=',
+      mcChange: '&'
     },
     controller: [function McDatesController() {
-      this.from = null;
-      this.to = null;
+
+      this.compare = function compare(date1, date2) {
+        if (date1 instanceof Date && date2 instanceof Date) {
+          return date1.toDateString() === date2.toDateString();
+        }
+
+        return date1 === date2;
+      };
+
+      this.set = function set(dateFrom, dateTo) {
+        if (!this.compare(this.dateFrom, dateFrom) || !this.compare(this.dateTo, dateTo)) {
+          this.dateFrom = dateFrom;
+          this.dateTo = dateTo;
+
+          this.change();
+        }
+      };
+
+      this.change = function change() {
+        setTimeout(() => this.mcChange(), 0);
+      };
 
       this.setYesterday = function setYesterday() {
-        this.from = new Date();
-        this.to = new Date();
+        const dateFrom = new Date();
+        const dateTo = new Date();
 
-        this.from.setDate(this.from.getDate() - 1);
-        this.to.setDate(this.to.getDate() - 1);
+        dateFrom.setDate(dateFrom.getDate() - 1);
+        dateTo.setDate(dateTo.getDate() - 1);
+
+        this.set(dateFrom, dateTo);
       };
 
       this.setToday = function setToday() {
-        this.from = new Date();
-        this.to = new Date();
+        this.set(new Date(), new Date());
       };
 
       this.setTwoWeeks = function setTwoWeeks() {
-        this.from = new Date();
-        this.to = new Date();
+        const dateFrom = new Date();
 
-        this.from.setDate(this.from.getDate() - 14);
+        dateFrom.setDate(dateFrom.getDate() - 14);
+
+        this.set(dateFrom, new Date());
       };
 
       this.setMonth = function setMonth() {
-        this.from = new Date();
-        this.to = new Date();
+        const dateFrom = new Date();
 
-        this.from.setDate(this.from.getDate() - 30);
+        dateFrom.setDate(dateFrom.getDate() - 30);
+
+        this.set(dateFrom, new Date());
       };
 
       this.clear = function clear() {
-        this.from = null;
-        this.to = null;
+        this.set(null, null);
       };
     }]
   });
